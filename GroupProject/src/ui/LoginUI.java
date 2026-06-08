cdf3adc5a7838c4f174287a883f147a21734f24a
xwx-da1sy <2717565@dundee.ac.uk>
2026-06-08 10:21:39 +0800

    登录界面的几个简单的组件.

--- GroupProject/src/ui/LoginUI.java
+++ GroupProject/src/ui/LoginUI.java
@@ -13,32 +13,21 @@ public class LoginUI extends JFrame {
         this.setSize(520, 360);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
 
         // 采用最为简单的棋盘布局，7行，2列
         panel = new JPanel(new GridLayout(7, 2));
 
-        panel.add(new JLabel("Network Name:"));
-        panel.add(new JTextField());
-
-        panel.add(new JLabel("Username:"));
-        panel.add(new JTextField());
-
-        panel.add(new JLabel("Password:"));
-        panel.add(new JPasswordField());
-
-        panel.add(new JLabel("HomeTown:"));
-        panel.add(new JTextField());
-
-        panel.add(new JLabel("WorkPlace:"));
-        panel.add(new JTextField());
-
-        panel.add(new JButton("Load Network"));
-        panel.add(new JButton("Create Network"));
-
-        panel.add(new JButton("Register User"));
-        panel.add(new JButton("Login"));
+        // JLabel就是一段显示文字这里创建它们的对象
+        JLabel networkNameLabel = new JLabel("Network Name:");
+        // JTextField就是一个输入框
+        JTextField networkNameTextField = new JTextField();
+        // 将第一行的文字和第一行的文本输入框放进panel中去
+        panel.add(networkNameLabel);
+        panel.add(networkNameTextField);
 
         this.add(panel);
+
+        // 将窗口设置为可见，这一步一定是在最后完成。
+        this.setVisible(true);
     }
 }
